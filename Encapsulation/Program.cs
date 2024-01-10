8553699627fe9fbf551c707e4d1ee96e63eeab28
Ashraf Alagmawy <ashrafemad097@gmail.com>
2024-01-10 22:07:57 +0200

    Adding More Logic

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Encapsulation
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+﻿using Encapsulation;
+
+var employee = new Employee();
+employee.FirstName = "Ashraf";
+employee.LastName = "Alagmawy";
+employee.BirthDate = new DateOnly(2001, 12, 21);
+employee.BasicSalary = 5000;
+employee.TaxPercentage = 140;
+
+Console.WriteLine($"Full Name:\t{employee.FirstName} {employee.LastName}");
+Console.WriteLine($"Birth Date:\t{employee.BirthDate}");
+Console.WriteLine($"Basic Salary:\t{employee.BasicSalary}");
+Console.WriteLine($"Tax Percentage:\t{employee.TaxPercentage}");
+Console.ReadKey();
