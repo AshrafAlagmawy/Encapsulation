1511ac5735919ff940c47ac16a4c95367dd6ffe4
Ashraf Alagmawy <ashrafemad097@gmail.com>
2024-01-10 21:58:52 +0200

    Add project files.

---
+++ Encapsulation/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Threading .Tasks;
+
+namespace Encapsulation
+{
+    internal class Employee
+    {
+        // Automatic Property {get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public decimal BasicSalary { get; set; }
+        public int TaxPercentage { get; set; }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
