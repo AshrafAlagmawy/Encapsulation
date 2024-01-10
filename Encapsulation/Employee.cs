1b518d9f7f41481b5d1187155da17501285b7be9
Ashraf Alagmawy <ashrafemad097@gmail.com>
2024-01-10 22:59:28 +0200

    Adding The Full Protections To All Attributes And Methods

--- Encapsulation/Employee.cs
+++ Encapsulation/Employee.cs
@@ -1,15 +1,67 @@
 ﻿using System.Text;
 using System.Threading .Tasks;
 
+/*
+ * The Main Goal Of Encapsulation 
+ 
+1. Hide Internal Details:
+    Encapsulation allows the bundling of data and methods within a class, hiding the internal details of an object and exposing only what is necessary.
+
+2. Data Hiding:
+    It provides a mechanism for hiding the implementation details of a class, enhancing security and reducing the risk of unintended interference.
+
+3. Modularity:
+    Encapsulation promotes code modularity by organizing code into self-contained units (classes), making it easier to manage and maintain.
+
+4. Abstraction:
+    It offers a level of abstraction by defining a clear interface for interacting with objects, allowing users to work with objects without needing to understand
+    their internal workings.
+
+5. Code Organization:
+    Encapsulation improves code organization by grouping attributes and methods related to a specific object within a class, enhancing code readability and
+    maintainability.
+
+6. Flexibility and Maintainability:
+    It allows for changes to the internal implementation of a class without affecting external code, improving flexibility and making code easier to maintain and extend.
+ */
+
+
 namespace Encapsulation
 {
     internal class Employee
     {
         // Automatic Property {get; set;}
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public decimal BasicSalary { get; set; }
-        public int TaxPercentage { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateOnly BirthDate { get; private set; }
+        public decimal BasicSalary { get; private set; }
+        public int TaxPercentage { get; private set; }
+
+        public void SetName(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Please, Enter Your Full Name First And Last Name");
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void SetBirthDate(DateOnly birthDate)
+        {
+            if (birthDate < new DateOnly(1986, 9, 17))
+                throw new ArgumentException("Invalid Birth Date !");
+            BirthDate = birthDate;
+        }
+        public void SetBasicSalary(decimal salary)
+        {
+            if (salary < 0)
+                throw new ArgumentException("Invalid Value Of Salary");
+            BasicSalary = salary;
+        }
+        public void SetTaxPercentage(int taxPercentage)
+        {
+            if (taxPercentage > 40 || taxPercentage < 10)
+                throw new ArgumentException("Invalid Percentage Of Tax Percentage");
+            TaxPercentage = taxPercentage;
+        }
+     
     }
 }
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,14 +1,32 @@
 ﻿using Encapsulation;
 
 var employee = new Employee();
-employee.FirstName = "Ashraf";
-employee.LastName = "Alagmawy";
-employee.BirthDate = new DateOnly(2001, 12, 21);
-employee.BasicSalary = 5000;
-employee.TaxPercentage = 140;
 
-Console.WriteLine($"Full Name:\t{employee.FirstName} {employee.LastName}");
+//employee.FirstName = "Ashraf";
+//employee.LastName = "Alagmawy";
+//employee.BirthDate = new DateOnly(2001, 12, 21);
+//employee.BasicSalary = 5000;
+//employee.TaxPercentage = 140;
+
+//Console.WriteLine($"Full Name:\t{employee.FirstName} {employee.LastName}");
+//Console.WriteLine($"Birth Date:\t{employee.BirthDate}");
+//Console.WriteLine($"Basic Salary:\t{employee.BasicSalary}");
+//Console.WriteLine($"Tax Percentage:\t{employee.TaxPercentage}");
+
+//employee.SetBirthDate(new DateOnly(1985, 12, 21));
+//Console.WriteLine($"Birth Date:\t{employee.BirthDate}");
+
+//employee.FirstName = "Ashraf"; Can't Access To The Value Of It Because It's Private On The Set Value
+
+employee.SetName("Ashraf","Alagmawy");
+employee.SetBirthDate(new DateOnly(2001, 12, 21));
+employee.SetBasicSalary(5000);
+employee.SetTaxPercentage(10);
+
+Console.WriteLine($"First Name:\t{employee.FirstName}");
+Console.WriteLine($"Last Name:\t{employee.LastName}");
 Console.WriteLine($"Birth Date:\t{employee.BirthDate}");
 Console.WriteLine($"Basic Salary:\t{employee.BasicSalary}");
-Console.WriteLine($"Tax Percentage:\t{employee.TaxPercentage}");
+Console.WriteLine($"TaxPercentage:\t{employee.TaxPercentage}");
+
 Console.ReadKey();
